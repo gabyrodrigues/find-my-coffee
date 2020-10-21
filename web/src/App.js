46b6bb58c5a657567a11100b46e8d7d061c71007
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-10-21 20:13:30 -0300

    Criando estrutura do projeto web, criando a index com o google maps + posição atual + listando cafeterias da api do google

---
+++ web/src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+import EstablishmentsService from './services/EstablishmentsService';
+
+function App() {
+	const { REACT_APP_GOOGLE_KEY } = process.env;
+
+	const [latitude, setLatitude] = useState(0);
+	const [longitude, setLongitude] = useState(0);
+	const [locations, setLocations] = useState([]);
+
+	useEffect(() => {
+		setCurrentLocation();
+	}, []);
+
+	async function loadCoffeeShops () {
+		const response = await EstablishmentsService.index(latitude, longitude);
+		setLocations(response.data.results);
+	}
+
+	async function setCurrentLocation () {
+		await navigator.geolocation.getCurrentPosition(function (position){
+			setLatitude(position.coords.latitude);
+			setLongitude(position.coords.longitude);
+			loadCoffeeShops(); 
+		}, function (error) {
+			alert("Habilite a localização para usar esta aplicação.")
+		});
+	}
+
+	return (
+		<>
+			<LoadScript googleMapsApiKey={REACT_APP_GOOGLE_KEY}>
+				<GoogleMap 
+					mapContainerStyle={{ height: "100vh", width: "100%" }}
+					zoom={15}
+					center={{ lat: latitude, lng: longitude }}
+				>
+					{
+						locations.map((item, index) => {
+							return (
+								<Marker 
+									key={index} 
+									icon="/images/coffee-pin.png"
+									title={item.name} 
+									animation="4" 
+									position={{ lat: item.geometry.location.lat, lng: item.geometry.location.lng }}
+								/>
+							);
+						})
+					}
+
+					<Marker 
+						key="Your location"
+						icon="images/my-location-pin.png"
+						title="Seu local"
+						animation="2"
+						position={{ lat: latitude, lng: longitude }}
+					/>
+				</GoogleMap>
+			</LoadScript>
+		</>
+	);
+}
+
+export default App;
---
+++ web/src/services/EstablishmentsService.js
@@ -0,0 +1,8 @@
+import api from './api';
+
+const EstablishmentsService = {
+    index: (latitude, longitude) => api.get(`google_stores?latitude=${latitude}&longitude=${longitude}`),
+    show: (place_id) => api.get(`/google_stores/${place_id}`)
+}
+
+export default EstablishmentsService;
